0e89cbc3d6f65dcb579c4a3d7cf367ca4f372671
TilloSulton <khikmat_tillo@mail.ru>
2021-02-08 04:44:24 +0500

    made registration

--- testapi/api/admin.py
+++ testapi/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Poll, Choice, Vote 
 
 # Register your models here.
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['question', 'created_by', 'pub_date']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
