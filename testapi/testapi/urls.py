2327c57d5dd016b58ddcffff0d7ec88f15cc3cf8
TilloSulton <khikmat_tillo@mail.ru>
2021-02-08 05:01:24 +0500

    add the urls of api

--- testapi/testapi/urls.py
+++ testapi/testapi/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import polls_detail, polls_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls', polls_list, name = 'polls_list'),
+    path('polls/<int:pk>/', polls_detail, name = 'polls_detail'),
+    
 ]
