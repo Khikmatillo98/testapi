df81f97dec0bbc89b021bcf147f39f94d2cd2e26
TilloSulton <khikmat_tillo@mail.ru>
2021-02-10 03:09:08 +0500

    API class orqali pk yani id ni foydalandim

--- testapi/api/views.py
+++ testapi/api/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
-from .models import Poll 
+from .models import Poll, Choice 
+from .serializers import PollSerializer 
 from django.http import JsonResponse 
+from rest_framework.views import APIView 
+from rest_framework.response import Response 
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 
 
-def polls_list(request):
-    MAX_OBJECTS = 20 
-    polls = Poll.objects.all()[:MAX_OBJECTS]
-    data = {'results': list(polls.values('question', 'created_by', 'pub_date'))}
-    return JsonResponse(data)
+
+class PollList(APIView):
+
+    def get(self, request):
+        polls = Poll.objects.all()[:20]
+        data = PollSerializer(polls, many=True).data
+        return Response(data)
 
 
-def polls_detail(request, pk):
-    poll = get_object_or_404(Poll, pk=pk)
-    data = {'results': {'question': poll.question,
-                        'created_by': poll.created_by.username, 
-                        'pub_date': poll.pub_date}}
 
+class PollDetail(APIView):
 
-    return JsonResponse(data)
+    def get(self, request, pk):
+        poll = get_object_or_404(Poll, pk=pk)
+        data = PollSerializer(poll).data 
+        return Response(data)
