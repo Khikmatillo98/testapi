3401d039fd24475d7dc986bd640d7603a8709278
TilloSulton <khikmat_tillo@mail.ru>
2021-02-10 03:19:15 +0500

    insted of API class, used generics module to simplify

--- testapi/api/views.py
+++ testapi/api/views.py
@@ -5,24 +5,26 @@ from django.http import JsonResponse
 from rest_framework.views import APIView 
 from rest_framework.response import Response 
 from django.shortcuts import get_object_or_404
-
+from rest_framework import generics 
 
 # Create your views here.
 
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+    # def get(self, request):
+    #     polls = Poll.objects.all()[:20]
+    #     data = PollSerializer(polls, many=True).data
+    #     return Response(data)
 
 
-class PollList(APIView):
 
-    def get(self, request):
-        polls = Poll.objects.all()[:20]
-        data = PollSerializer(polls, many=True).data
-        return Response(data)
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
-
-
-class PollDetail(APIView):
-
-    def get(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        data = PollSerializer(poll).data 
-        return Response(data)
+    # def get(self, request, pk):
+    #     poll = get_object_or_404(Poll, pk=pk)
+    #     data = PollSerializer(poll).data 
+    #     return Response(data)
